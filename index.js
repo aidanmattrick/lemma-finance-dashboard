e4b1fa54d88974d4e96032fe9a5f395f006e8c62
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-15 11:53:08 -0700

    Changing export type to mod instead of fx

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bucket = storage.bucket('lemma_dash_test');
 // import { writeToParquet } from '/src/index.ts';
 // commenting out in case GCF issue with TS
 
-export function writeRawData() {
+function writeRawData() {
   bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
 });
+}
+
+module.exports = {
+  writeRawData
 }
