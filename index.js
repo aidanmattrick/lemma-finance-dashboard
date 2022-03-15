233bfdbb8928389f1286bd2f79d8e0ff1b42ab53
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-15 12:15:29 -0700

    Changing export type

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const bucket = storage.bucket('lemma_dash_test');
 // import { writeToParquet } from '/src/index.ts';
 // commenting out in case GCF issue with TS
 
-function writeRawData() {
-  bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
-});
-}
+// function writeRawData() {
+//   bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
+// });
+// }
 
-module.exports = {
-  writeRawData
+// module.exports = {
+//   writeRawData
+// }
+
+exports.writeRawData=function(){
+  bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
+  });
 }
