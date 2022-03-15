bf3928f2607bbdc05ef5d06c07b0a7faf7229b31
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-15 09:17:36 -0700

    Removing TS in case

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
 const bucket = storage.bucket('lemma_dash_test');
-import { writeToParquet } from '/src/index.ts';
+// import { writeToParquet } from '/src/index.ts';
+// commenting out in case GCF issue with TS
 
 export function writeRawData() {
   bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
