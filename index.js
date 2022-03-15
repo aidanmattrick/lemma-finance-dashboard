6aa6e833ebb58a908908ede9d843bcc48bed760c
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-15 08:38:28 -0700

    fixed typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const storage = new Storage();
 const bucket = storage.bucket('lemma_dash_test');
 import { writeToParquet } from '/src/index.ts';
 
-exports.function writeRawData() {
+export function writeRawData() {
   bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
 });
 }
