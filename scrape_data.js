8a4876602e7badd28cc2cf417a22198239432b50
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-15 08:56:19 -0700

    Removed file

--- scrape_data.js
+++
@@ -1,9 +0,0 @@
-const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
-const bucket = storage.bucket('lemma_dash_test');
-import { writeToParquet } from '/src/index.ts';
-
-export function writeRawData() {
-  bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
-});
-}
