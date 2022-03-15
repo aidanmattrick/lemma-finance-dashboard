8bf614bb5004ace67c557dad3f3a0356e4e64823
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-15 12:23:27 -0700

    Changing export fx to arrow

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const bucket = storage.bucket('lemma_dash_test');
 //   writeRawData
 // }
 
-exports.writeRawData=function(){
+exports.writeRawData=()=>{
   bucket.upload('data/results/viz_df_daily_03-10-22.parquet', function(err, file) {
   });
 }
