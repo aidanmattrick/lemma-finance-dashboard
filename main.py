579b7af9b511b6d52fd6556e960e93e4719aef3a
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-23 16:56:58 -0700

    Running based on evt change not testing now

--- main.py
+++ main.py
@@ -22,33 +22,36 @@ def append_to_raw_main():
 def test(event, context):
     print(event)
     #FOR TESTING:
-    print('USDLemma_raw_latest updated...')
-    raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
-    if raw_latest_df.shape[0] < 2:
-        print('No new data was uploaded to USDLemma_raw_latest.')
-        print('Wrapping up function.')
-        return
-    else:
-        covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
-        print('New data being processed...')
-        raw_df = append_to_raw_main()
-        #raw_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
-        process_data(raw_df, covalent_api_key)
-        print('Main loop finished executing.')
+    # print('USDLemma_raw_latest updated...')
+    # raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
+    # if raw_latest_df.shape[0] < 2:
+    #     print('No new data was uploaded to USDLemma_raw_latest.')
+    #     print('Wrapping up function.')
+    #     return
+    # else:
+    #     covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
+    #     print('New data being processed...')
+    #     raw_df = append_to_raw_main()
+    #     #raw_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
+    #     process_data(raw_df, covalent_api_key)
+    #     print('Main loop finished executing.')
+    #     return
 
     # #REALTIME
-    # if event['name'] == 'USDLemma_raw_latest':
-    #     print('USDLemma_raw_latest updated...')
-    #     raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
-    #     if raw_latest_df.shape[0] < 2:
-    #         print('No new data was uploaded to USDLemma_raw_latest.')
-    #         print('Wrapping up function.')
-    #         return
-    #     else:
-    #         print('New data being processed...')
-    #         raw_df = append_to_raw_main()
-    #         #raw_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
-    #         process_data(raw_df)
-    #         print('Main loop finished executing.')
+    if event['name'] == 'USDLemma_raw_latest':
+        print('USDLemma_raw_latest updated...')
+        raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
+        if raw_latest_df.shape[0] < 2:
+            print('No new data was uploaded to USDLemma_raw_latest.')
+            print('Wrapping up function.')
+            return
+        else:
+            print('New data being processed...')
+            covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
+            raw_df = append_to_raw_main()
+            #raw_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
+            process_data(raw_df, covalent_api_key)
+            print('Main loop finished executing.')
+            return
 
 
