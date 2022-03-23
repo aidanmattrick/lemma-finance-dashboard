c16f942b7875eec1400bba2fce16fd015bc9b5d3
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-22 18:22:00 -0700

    Changing evt property

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #https://cloud.google.com/functions/docs/writing/background#cloud-storage-example
 def test(event, context):
-    print(event['name'])
+    print(event)
     print(context)
     df = pd.read_parquet('gs://lemma_dash_test/USALemma_raw_latest.parquet')
     print(df.shape)
