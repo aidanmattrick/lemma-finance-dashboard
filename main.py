6f29c1957ded99746ce99aa19d781feaa4b5e4ed
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-03-23 19:24:58 -0700

    Catching right event

--- main.py
+++ main.py
@@ -20,9 +20,8 @@ def append_to_raw_main():
 
 #RENAME TO MAIN
 def test(event, context):
-    print(event)
-    # #REALTIME
-    if event['name'] == 'USDLemma_raw_latest':
+    #REALTIME
+    if event['name'] == 'USDLemma_raw_latest.parquet':
         print('USDLemma_raw_latest updated...')
         raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
         if raw_latest_df.shape[0] < 2:
@@ -33,10 +32,11 @@ def test(event, context):
             print('New data being processed...')
             covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
             raw_df = append_to_raw_main()
-            #raw_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
             process_data(raw_df, covalent_api_key)
             print('Main loop finished executing.')
             return
+    else:
+        print('Another event, not trigger happened')
 
 #FOR TESTING:
 # print('USDLemma_raw_latest updated...')
