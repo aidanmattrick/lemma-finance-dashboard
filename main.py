bd20ba98b69ff8918b2a26a17cf2d1e04be2893e
aidanmattrick <40402840+aidanmattrick@users.noreply.github.com>
2022-04-04 09:50:22 -0700

    Debugging price API issue

--- main.py
+++ main.py
@@ -20,32 +20,32 @@ def append_to_raw_main():
 
 def main_loop(event, context):
     #REALTIME:
-    if event['name'] == 'USDLemma_raw_latest.parquet':
-        print('USDLemma_raw_latest updated...')
-        raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
-        if raw_latest_df.shape[0] < 2:
-            print('No new data was uploaded to USDLemma_raw_latest.')
-            print('Wrapping up function.')
-            return
-        else:
-            print('New data being processed...')
-            covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
-            raw_df = append_to_raw_main()
-            process_data(raw_df, covalent_api_key)
-            print('Main loop finished executing.')
-            return
-    else:
-        print('Another event, not trigger happened')
-        return
+    # if event['name'] == 'USDLemma_raw_latest.parquet':
+    #     print('USDLemma_raw_latest updated...')
+    #     raw_latest_df = pd.read_parquet('gs://lemma_dash/USDLemma_raw_latest.parquet')
+    #     if raw_latest_df.shape[0] < 2:
+    #         print('No new data was uploaded to USDLemma_raw_latest.')
+    #         print('Wrapping up function.')
+    #         return
+    #     else:
+    #         print('New data being processed...')
+    #         covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
+    #         raw_df = append_to_raw_main()
+    #         process_data(raw_df, covalent_api_key)
+    #         print('Main loop finished executing.')
+    #         return
+    # else:
+    #     print('Another event, not trigger happened')
+    #     return
 
     #FOR TESTING:
-    # print('Starting testing loop...')
-    # covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
-    # print('New data being processed...')
-    # raw_df = append_to_raw_main()
-    # print('Raw appended.')
-    # process_data(raw_df, covalent_api_key)
-    # print('Main loop finished executing.')
-    # return
+    print('Starting testing loop...')
+    covalent_api_key = pd.read_csv('gs://lemma_dash_api_key/api_key.csv')['covalent_api_key'][0]
+    print('New data being processed...')
+    raw_df = append_to_raw_main()
+    print('Raw appended.')
+    process_data(raw_df, covalent_api_key)
+    print('Main loop finished executing.')
+    return
 
 
